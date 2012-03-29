62ea5bf37326a4751d10ab365ca493906c088d07
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-03-29 12:04:02 -0400

    added a bunch of interpolation figures to interpolation.tex

--- condor/generate_submit_file.py
+++ condor/generate_submit_file.py
@@ -26,7 +26,7 @@ WhenToTransferOutput = ON_EXIT
 notify_user = kyle.t.konrad@gmail.com
 
 transfer_input_files = verg, count, dummy
-transfer_output_files = dummy
+transfer_output_files = dummy, *.sta_bin
 
 Executable = vc
 
