414fb3f662815d3a44bc4c323645214f755e8950
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-04-25 16:57:23 -0400

    fix count_all to use constant alpha rather than h

--- c/count_all.py
+++ c/count_all.py
@@ -3,11 +3,14 @@
 import os
 import re
 
+alpha = 0.5
+
 path = '/media/My Passport/sta'
 escaped_path = '/media/My\ Passport/sta'
 for f in os.listdir(path):
     match = re.match('run_(.*)\.sta_bin', f)
     k = match.groups(1)[0]
-    cmd = './count -f %s/%s -l qugrs:1.0:0.4:0.7 -d 0.001 -M 9 -u 20 -k %s >> counts.txt' % (escaped_path, f, k)
+    h = alpha/k
+    cmd = './count -f %s/%s -l qugrs:1.0:0.4:0.7 -d %f -M 9 -u 20 -k %s >> counts.txt' % (escaped_path, f, h, k)
     print cmd
     os.system(cmd)
