d8b65fd298928cc33b2ac803b1c8b8d770a8528b
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-05-01 22:43:19 -0400

    check for already counted eigenfunctions in count_all.py

--- c/count_all.py
+++ c/count_all.py
@@ -5,12 +5,18 @@ import re
 import sys
 
 if len(sys.argv) < 3:
-    print 'usage: count_all.py path_to_sta_bin_files output_file'
+    print 'usage: count_all.py path_to_sta_bin_files output_file [already_counted_file]'
     exit(-1)
 
 path = sys.argv[1]
 escaped_path = re.sub(' ', '\ ', path)
 outfile = sys.argv[2]
+already_counted_ks = []
+
+if len(sys.argv) >= 4:
+    with open(sys.argv[3]) as f:
+        for line in f:
+            already_counted_ks.append(float(line.split(',')[0]))
 
 alpha = 0.5
 for f in os.listdir(path):
@@ -18,6 +24,9 @@ for f in os.listdir(path):
     if not match:
         raise ValueError('failed to parse filename: %s' % f)
     k = float(match.groups(1)[0])
+    if k in already_counted_ks:
+        print 'already counted %f' % k
+        continue
     h = alpha / k
     cmd = './count -f %s/%s -l qugrs:1.0:0.4:0.7 -d %f -M 9 -u 20 -k %f >> %s' % (escaped_path, f, h, k, outfile)
     print cmd
