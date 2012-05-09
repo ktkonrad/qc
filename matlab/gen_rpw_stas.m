ec3ffbf12a30d118121e3b27f05a068b1c297ce6
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-05-09 19:55:38 -0400

    updated gen_rpw_stas

--- matlab/gen_rpw_stas.m
+++ matlab/gen_rpw_stas.m
@@ -17,18 +17,20 @@ dx = L./n;
 k = 2*pi*n./(L.*ppw);  % should all be 1.0
 
 %% specify ppw
-ppw = [2:10 12:2:18 20:6:44 50:10:80];
+ppw = [2:10 12:2:18 20:6:44 50:10:90 100:20:160];
 n0 = 20;
 n = n0 .* ppw ./ ppw(1);
 L = 2*pi./ppw .* n; % width of window
 dx = L./n;
 k = 2*pi*n./(L.*ppw);
 
+%% run 100 times
 N = 100;
+interp_counts= zeros(numel(ppw), N);
+no_interp_counts= zeros(numel(ppw), N);
 interp_errors = zeros(numel(ppw), N);
 no_interp_errors = zeros(numel(ppw), N);
 for j=1:N
-%% generate rpws and save as sta_bin
 [fs, xs] = rpw2dsample_multi(n0, ppw);
 
 for i=1:numel(fs)
@@ -38,22 +40,23 @@ for i=1:numel(fs)
     save_sta(sta, k(i), sprintf('../data/rpw_%.8f', dx(i)));
 end
 
-%% count nodal domains
 system('../scripts/count_rpws.py ../data');
 
-%% plot results
 interp_stats = dlmread('rpw_counts_interp.txt');
 no_interp_stats = dlmread('rpw_counts_no_interp.txt');
 alpha = interp_stats(:,1) .* interp_stats(:,2);
-interp_counts = interp_stats(:,3);
-no_interp_counts = no_interp_stats(:,3);
+interp_counts(:,j) = interp_stats(:,3);
+no_interp_counts(:,j) = no_interp_stats(:,3);
 
 true_count = interp_counts(1);
-interp_errors(:,j) = abs(interp_counts - true_count) ./ true_count;
-no_interp_errors(:,j) = abs(no_interp_counts - true_count) ./ true_count;
+interp_errors(:,j) = abs(interp_counts(:,j) - true_count) ./ true_count;
+no_interp_errors(:,j) = abs(no_interp_counts(:,j) - true_count) ./ true_count;
 
-%figure;
-%plot(alpha, interp_errors);
-%hold on;
-%plot(alpha, no_interp_errors, 'r');
+save('rpw_interp.mat', 'interp_counts', 'no_interp_counts', 'interp_errors', 'no_interp_errors');
 end
+
+%% plot
+figure;
+loglog(alpha, mean(interp_errors,2));
+hold on;
+loglog(alpha, mean(no_interp_errors,2), 'r');
