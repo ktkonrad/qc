e3de1fd3646af79a4358da32c19be3eff1efc3c8
Kyle Konrad <kyle.t.konrad@gmail.com>
2012-05-14 13:13:25 -0400

    added option to generate range of ranges of k

--- condor/generate_submit_file.py
+++ condor/generate_submit_file.py
@@ -17,6 +17,23 @@ def krange(k_low, k_high, delta_k):
         yield (k, k+2*delta_k)
         k += 2*delta_k
 
+def kmetarange(k_low, k_high, delta_k, meta_width, meta_delta):
+    """
+    divide [k_low, k_high] into intervals of width meta_width*(k_low/k)**2
+    (this width scaling yields a constant number of eigenvalues in each large interval)
+    and spacing meta_delta
+    divide these intervals into adjacent intervals of width 2*delta_k
+    yield pairs (k1, k2) defining the endpoints of these small intervals
+
+    """
+    meta_k = k_low
+    while meta_k < k_high:
+        k = meta_k
+        while k < min(meta_k + meta_width*(k_low/meta_k)**2, k_high):
+            yield (k, k+2*delta_k)
+            k += 2*delta_k
+        meta_k += meta_delta
+
 def find_gaps(delta, counts_file):
     """find k gaps in a file and yield k ranges of jobs to fill the gaps"""
     last_k = float('inf') # start last k at infinity to guarantee gap < MAX_GAP on first iteration
@@ -77,6 +94,12 @@ def write_submit_file(k_low, k_high, delta, billiard):
         for (k1, k2) in krange(k_low, k_high, delta):
             write_job(submit_file, k1, k2, billiard)
 
+def write_metarange_submit_file(k_low, k_high, delta, meta_width, meta_delta, billiard):
+    with open("verg_and_count.submit", 'w') as submit_file:
+        write_prelude(submit_file)
+        for (k1, k2) in kmetarange(k_low, k_high, delta, meta_width, meta_delta):
+            write_job(submit_file, k1, k2, billiard)
+
 def write_filtered_submit_file(k_low, k_high, delta, billiard):
     with open("verg_and_count.submit", 'w') as submit_file:
         write_prelude(submit_file)
@@ -110,9 +133,15 @@ def main():
     if len(sys.argv) == 5:
         write_submit_file(k_low, k_high, delta_k, billiard)
     elif len(sys.argv) == 6 and sys.argv[5] == 'f':
-            write_filtered_submit_file(k_low, k_high, delta_k, billiard)
+        write_filtered_submit_file(k_low, k_high, delta_k, billiard)
     elif len(sys.argv) == 7 and sys.argv[5] == 'g':
-            write_gaps_submit_file(delta_k, billiard, sys.argv[6])
+        counts_file = sys.argv[6]
+        write_gaps_submit_file(delta_k, billiard, counts_file)
+    elif len(sys.argv) == 8 and sys.argv[5] == 'm':
+        metarange_width = float(sys.argv[6])
+        metarange_delta = float(sys.argv[7])
+        write_metarange_submit_file(k_low, k_high, delta_k, metarange_width, metarange_delta, billiard)
+
     else:
         usage()
         exit(-1)
